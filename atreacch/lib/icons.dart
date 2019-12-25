95ed4fb5d64f0ec4bb5372dec9a58b684920e9b8
DhruvilPatel <atdhruvilpatel@gmail.com>
2019-12-25 16:13:48 -0500

    initial commit

--- atreacch/lib/icons.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class HackRUIcons{
-  static final IconData menu =
-  new IconData(0xe80d, fontFamily: 'Fontello');
-}
---
+++ humanitarian_icons/test/humanitarian_icons_test.dart
@@ -0,0 +1,5 @@
+/*
+
+CHECK OUT example/ project!!
+
+*/
